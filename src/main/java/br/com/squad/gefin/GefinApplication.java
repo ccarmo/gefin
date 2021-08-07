61bb9a74cdc5a397b3fa54f1d22d919b3a6fe43d
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-08-06 22:44:25 -0300

    Commit inicial

---
+++ src/main/java/br/com/squad/gefin/GefinApplication.java
@@ -0,0 +1,13 @@
+package br.com.squad.gefin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GefinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GefinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/squad/gefin/GefinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.squad.gefin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GefinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
